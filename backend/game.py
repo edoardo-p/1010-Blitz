7359b8ceeb0a57684165ed1bc05606e166357fea
edoardo-p <edoardo.protti@gmail.com>
2023-11-23 10:41:10 +0100

    Moved piece generation to game class

--- backend/game.py
+++ backend/game.py
@@ -1,46 +1,71 @@
-from frontend.piece import Piece
-from frontend.tile import Tile
+import json
+import random
+
+from .piece import Piece
+from .tile import Tile
 
 
 class Game:
     def __init__(self, board_size: int):
         self.score = 0
         self.board_size = board_size
-        self.tiles = [[Tile() for _ in range(board_size)] for _ in range(board_size)]
+
+        with open(r".\backend\vectors.json", "r") as f:
+            self._piece_vectors = json.load(f)
+        del f
+
+        self.pieces = self._generate_pieces()
+        self._tiles = [[Tile() for _ in range(board_size)] for _ in range(board_size)]
 
     def update(self, row: int, col: int, piece: Piece) -> bool:
-        if self._check_valid(row, col, piece):
-            for tile_col, tile_row in piece.squares_pos:
-                self.tiles[row + tile_row][col + tile_col].update(piece.color)
-            self._clear_lines()
-            self.score += len(piece.squares_pos)
-            return True
+        if not self._check_valid(row, col, piece):
+            return False
 
-        return False
+        for tile_col, tile_row in piece.squares_pos:
+            self._tiles[row + tile_row][col + tile_col].update(piece.color)
+        self._clear_lines()
+        self.score += len(piece.squares_pos)
+        return True
 
-    def has_lost(self, pieces: list[Piece]) -> bool:
-        return not any(self.get_moves(pieces))
+    def has_lost(self) -> bool:
+        return not any(self.get_moves())
 
-    def get_moves(self, pieces: list[Piece]):
+    def get_piece_at_slot(self, slot: int) -> Piece | None:
+        if slot >= len(self.pieces):
+            return None
+
+        piece = self.pieces.pop(slot)
+        if not self.pieces:
+            self.pieces = self._generate_pieces()
+        return piece
+
+    def get_moves(self):
         for tile, row, col in self.get_tiles_and_coords():
             if not tile.empty:
                 continue
 
-            for piece in pieces:
-                if self._check_valid(row, col, piece):
+            for piece in self.pieces:
+                if piece and self._check_valid(row, col, piece):
                     yield piece, row, col
 
     def get_tiles_and_coords(self):
-        for i, row in enumerate(self.tiles):
+        for i, row in enumerate(self._tiles):
             for j, tile in enumerate(row):
                 yield tile, i, j
+
+    def _generate_pieces(self) -> list[Piece]:
+        pieces = []
+        for _ in range(3):
+            piece = random.choice(self._piece_vectors)
+            pieces.append(Piece(piece["pos"], piece["color"]))
+        return pieces
 
     def _check_valid(self, row: int, col: int, piece: Piece) -> bool:
         for tile_col, tile_row in piece.squares_pos:
             if not (
                 0 <= row + tile_row < self.board_size
                 and 0 <= col + tile_col < self.board_size
-                and self.tiles[row + tile_row][col + tile_col].empty
+                and self._tiles[row + tile_row][col + tile_col].empty
             ):
                 return False
 
@@ -66,7 +91,7 @@ class Game:
         self.score += 5 * lines * (lines + 1)
 
     def _get_row(self, row: int) -> list[Tile]:
-        return self.tiles[row]
+        return self._tiles[row]
 
     def _get_col(self, col: int) -> list[Tile]:
-        return [row[col] for row in self.tiles]
+        return [row[col] for row in self._tiles]
--- main.py
+++ main.py
@@ -1,28 +1,7 @@
-import json
-import random
-
 import pygame
 
 from backend.game import Game
 from frontend import GRID_HEIGHT, GRID_X, GRID_Y, TILE_SIZE, WIN_HEIGHT, WIN_WIDTH, gui
-from frontend.piece import Piece
-
-with open(r"frontend\vectors.json", "r") as f:
-    piece_vectors = json.load(f)
-del f
-
-
-def mask_pieces(pieces: list[Piece], mask: list[bool]) -> list[Piece]:
-    return [piece for piece, available in zip(pieces, mask) if available]
-
-
-def generate_pieces() -> list[Piece]:
-    pieces = []
-    for _ in range(3):
-        piece = random.choice(piece_vectors)
-        pieces.append(Piece(piece["pos"], piece["color"]))
-
-    return pieces
 
 
 def convert(x: int, y: int) -> tuple[int, int]:
@@ -37,8 +16,6 @@ def main():
 
     game = Game(board_size=10)
     is_holding = False
-    pieces = generate_pieces()
-    available_slots = [True, True, True]
 
     while True:
         screen.fill(0)
@@ -52,20 +29,14 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN and not is_holding:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
                 if GRID_HEIGHT + GRID_Y <= mouse_y <= WIN_HEIGHT:
-                    slot = 3 * mouse_x // WIN_WIDTH
-                    if available_slots[slot]:
-                        piece = pieces[slot]
-                        available_slots[slot] = False
+                    piece = game.get_piece_at_slot(3 * mouse_x // WIN_WIDTH)
+                    if piece:
                         is_holding = True
 
             elif event.type == pygame.MOUSEBUTTONDOWN and is_holding:
                 row, col = convert(*pygame.mouse.get_pos())
                 if game.update(row, col, piece):
-                    if not any(available_slots):
-                        pieces = generate_pieces()
-                        available_slots = [True, True, True]
-
-                    if game.has_lost(mask_pieces(pieces, available_slots)):
+                    if game.has_lost():
                         pygame.quit()
                         print(f"Final score: {game.score}")
                         return
@@ -76,7 +47,7 @@ def main():
             piece.update(*pygame.mouse.get_pos())
             gui.draw_piece(screen, piece)
 
-        gui.draw_piece_menu(screen, pieces, available_slots)
+        gui.draw_piece_menu(screen, game.pieces)
         pygame.display.flip()
 
 
