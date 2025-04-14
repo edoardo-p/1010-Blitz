e179d1c4cd479a5a1563200a99560d26a8539ab4
edoardo-p <61146513+edoardo-p@users.noreply.github.com>
2025-04-15 00:08:39 +0200

    add max_pieces param to gym register

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,5 +3,5 @@ import gymnasium as gym
 gym.register(
     id="custom_env/Game1010-v0",
     entry_point="backend.custom_env:Game1010",
-    kwargs={"board_size": 10},
+    kwargs={"board_size": 10, "max_pieces": 3},
 )
