86961ed8581fa6fb096f6a286d17706f25af1945
edoardo-p <61146513+edoardo-p@users.noreply.github.com>
2025-04-15 00:11:32 +0200

    improved type annotations for memory.py

--- backend/memory.py
+++ backend/memory.py
@@ -9,23 +9,25 @@ import torch
 class Transition:
     state: tuple[torch.Tensor, torch.Tensor]
     action: torch.Tensor
-    next_state: tuple[torch.Tensor, torch.Tensor]
+    next_state: torch.Tensor | None
     reward: torch.Tensor
-
-    def __iter__(self):
-        return iter((self.state, self.action, self.next_state, self.reward))
 
 
 class ReplayMemory:
-    def __init__(self, capacity):
+    def __init__(self, capacity: int):
         self.memory = deque([], maxlen=capacity)
 
-    def push(self, *args):
-        """Save a transition"""
-        self.memory.append(Transition(*args))
+    def push(
+        self,
+        state: tuple[torch.Tensor, torch.Tensor],
+        action: torch.Tensor,
+        next_state: torch.Tensor | None,
+        reward: torch.Tensor,
+    ) -> None:
+        self.memory.append(Transition(state, action, next_state, reward))
 
-    def sample(self, batch_size):
+    def sample(self, batch_size: int) -> list[Transition]:
         return random.sample(self.memory, batch_size)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.memory)
